d8895121c508af9e82665cfdb5b4ed1b20f05066
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-10-26 18:01:24 -0400

    added background

--- src/components/hero/heroContent.js
+++ src/components/hero/heroContent.js
@@ -1,10 +1,12 @@
 import React from "react"
 import styles from "./heroContent.module.css"
 import HeroFooter from "./heroFooter"
+import Background from "../background"
 
 const HeroContent = () => {
   return (
     <div className={styles.contentWrapper}>
+      <Background />
       <h2 className={styles.contentLine1}>HI I'M</h2>
       <h1 className={styles.contentLine2}>RAHAT</h1>
       <p className={styles.contentLine3}>
