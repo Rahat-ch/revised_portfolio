f7266c60b74b70d2471999473042c2b0b45e05a2
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-11-02 01:23:29 -0400

    projects listing page with desktop styles

---
+++ src/components/projectListing/index.js
@@ -0,0 +1,61 @@
+import React from "react"
+import styles from "./index.module.css"
+import Img from "gatsby-image"
+import classNames from "classnames"
+import BackgroundRight from "../background-project-right"
+import BackgroundLeft from "../background-project-left"
+
+const ProjectListing = ({ data }) => {
+  const projectIndex = data.allProjectsJson.edges
+  return (
+    <div className={styles.outerContainer}>
+      {projectIndex.map(({ node: item }) => {
+        const title = item.name
+        const description = item.description
+        const username = item.username
+        const password = item.password
+        const align = item.align
+        const imageData = item.image.childImageSharp.fluid
+        const hide = () => {
+          if (username.length > 0) {
+            return true
+          }
+        }
+
+        return (
+          <div key={title} className={styles[`projectWrapper${align}`]}>
+            <div className={styles.contentWrapper}>
+              <div className={styles.titleWrapper}>
+                <h3 className={styles.title}>{title}</h3>
+                <hr className={styles.horizontal} />
+              </div>
+              <p className={styles.description}>{description}</p>
+              <div
+                className={classNames({
+                  [styles.hide]: !hide(),
+                  [styles.content]: hide(),
+                })}
+              >
+                <p>Demo Login Information</p>
+                <p>Username: {username}</p>
+                <p>Password: {password}</p>
+              </div>
+              <div className={styles.projectFooter}>
+                <hr className={styles.horizontalFooter} />{" "}
+                <h4 className={styles.projectLink}>View Project</h4>
+              </div>
+            </div>
+            <Img
+              fluid={imageData}
+              alt={title}
+              className={styles[`image${align}`]}
+            />
+            {align == "right" ? <BackgroundRight /> : <BackgroundLeft />}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default ProjectListing
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,39 @@
 import React from "react"
 import NavBar from "../components/navbar/"
+import { graphql } from "gatsby"
+import ProjectListing from "../components/projectListing"
 
-const projectPage = () => {
+const projectPage = ({ data }) => {
   return (
     <div>
       <NavBar />
-      This is Projects
+      <ProjectListing data={data} />
     </div>
   )
 }
+
+export const query = graphql`
+  query PROJECTLISTQUERY {
+    allProjectsJson {
+      edges {
+        node {
+          id
+          name
+          description
+          username
+          password
+          align
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid_tracedSVG
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default projectPage
